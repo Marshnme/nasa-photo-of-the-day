f260614257e5b4f8272cdf1737a16ab2d5a9987a
Joshua Holtsclaw <Jildsmiles@yahoo.com>
2019-09-11 18:51:48 -0400

    data obtained. build out components more

--- src/components/DataCard.js
+++ src/components/DataCard.js
@@ -1,3 +1,16 @@
 import React from "react";
 
-const 
+ const DataCard = props =>{
+     console.log(props);
+     return (
+        <div>
+            <img alt ="planets" src={props.img}>
+            </img>
+            <h2>{props.name}</h2>
+
+        </div>
+     )
+ }
+
+
+export default DataCard;
--- src/components/DataList.js
+++ src/components/DataList.js
@@ -4,14 +4,26 @@ import DataCard from "./DataCard";
 
 
 function DataList (){
-
-
+    const [planet, setPlanet] = useState([]);
     useEffect(() => {
         axios
-        .get("https://api.nasa.gov/planetary/apod")
-        .then(repsonse =>{
-            console.log(repsonse;)
-        })
-    })
+            .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+            .then(response => {
+            
+            console.log("response data:", response);
+            setPlanet(planet);
+            
+          })
+          .catch(error => {
+            console.log("data was not returned", error);
+          });
+      }, [planet]);
+      return (
+          <div className="container">
+              <DataCard title={title} />
 
+          </div>
+      )
 }
+ 
+export default DataList;
