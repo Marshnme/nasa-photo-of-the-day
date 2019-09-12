863202953c18f9bdab66323129c53c7683d31f5f
Joshua Holtsclaw <Jildsmiles@yahoo.com>
2019-09-12 18:47:21 -0400

    Added basic buttons. implemented flexbox.

--- src/components/DataList.js
+++ src/components/DataList.js
@@ -1,14 +1,25 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import DataCard from "./DataCard";
+import styled from "styled-components";
+
+const ToggleNight = styled.button`
+background: black;
+
+
+`;
+
+
 
 
 function DataList (){
     const [planet, setPlanet] = useState("");
+    const [color, setColor]  = useState("white");
     useEffect(() => {
         axios
             .get("https://api.nasa.gov/planetary/apod?api_key=GDF98vMAtY1KV4shqkMeErNttoYSkrGkGxw2f5uE")
             .then(response => {
+                
             setPlanet(response.data);
             console.log("response data:", response.data);
         })
@@ -17,18 +28,24 @@ function DataList (){
         });
         }, []);
 
-
+    
+    
     return (
         <div className="container">
-            <DataCard 
+           
+            <ToggleNight onClick={() => setColor("black")}>black</ToggleNight>
+            <ToggleNight onClick={() => setColor("white")}>white</ToggleNight>
+
+            <div>
+                <DataCard 
                 title = {planet.title}
                 url = {planet.url}
                 date = {planet.date}
-                explanation = {planet.explanation}
-                copyright = {planet.copyright}
-
-            />
-        </div>
+                explanation={planet.explanation}
+                copyright = {planet.copyright}>           </DataCard>
+                </div>
+            </div>
+        
     )
 }
 
